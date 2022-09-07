233bbe27b5bfadbb934713c41807dcd185eb3898
Laura Luna <lauraluna.siqueira@hotmail.com>
2022-09-07 14:12:59 -0300

    first actions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import logo from './logo.svg'
+import email from './email.svg'
+import password from './password.svg'
 import './App.css';
+import './BoxLogin.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="NavBar"></div>
+
+        <img src={logo}></img>
+
+      <div className="BoxLogin">
+       
+          <h1>Bem Vindo de volta</h1>
+          <p id="text">Insira seu email e senha</p>
+        
+        
+          <input  placeholder="Email" id="email" ></input>
+
+            <br></br>
+
+          <input placeholder="Password" id="password" ></input>
+
+            <br></br>
+
+          <button className="SignIn">Sign In</button>
+
+          <p id="textsecund">Forget your password? Click here</p>
+          </div>
+        
+
+    
     </div>
   );
 }
+
+
 
 export default App;
