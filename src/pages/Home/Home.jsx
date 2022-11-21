8b8e51dd2cce6ff1df911f6631717adb23834ab8
Yuri12 <yuri.00000830267@unicap.br>
2022-11-21 16:29:50 -0300

    EsconderDiv Calendario

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -34,7 +34,15 @@ const ano = data.getFullYear();
   const [objReuniao, setObjReuniao] = useState(Modeloreuniao);
   //const[buttondisabled,setbuttondisabled]= useState();
 
+  const[MostrarDiv,setMostrarDiv]=useState(false);
+  const[EsconderDiv,setEsconderDiv]=useState(true);
+  //const[buttondisabled,setbuttondisabled]= useState();
 
+
+  const handleClickData=()=>{
+    setEsconderDiv(false)
+    setMostrarDiv(true)
+  }
 
 
 
@@ -103,17 +111,26 @@ const ano = data.getFullYear();
     <div className="pages-home">
       <img src={logoHeader} className="Logosecund"></img>
       <hr className="line"></hr>
-      <div className="Calendario">
+
+      {EsconderDiv &&(<div className="Calendario">
         <p>Adicione a data de preferência</p>
+        <input type="date" className="date" value={reuniao.moment} onChange={aoDigitar} name='moment' min={SubDate()}  ></input>
+
+
+        <br></br>
+        <a>Essa data está disponível</a>
+        <br></br>
+      
         <input type="date" className="date" value={reuniao.moment} onChange={aoDigitar} name='moment' min={SubDate()}  ></input>
         
        
         <br></br>
         <a>Essa data está disponível</a>
         <br></br>
-        <button className="Add" onClick={Cadastrar} onAuxClick={limparFormulario} value='Cadastrar'>Criar uma nova reunião</button>
-      </div>
-
+        <button className="Add" onClick={handleClickData} onAuxClick={limparFormulario} value='Cadastrar'>Criar uma nova reunião</button>
+      </div>)
+         }
+         
       <div className="add">
         <img src={add} id="add"></img>
 
